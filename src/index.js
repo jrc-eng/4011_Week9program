fd1be0274447c6cef393c1c70a58429ff3e9fb44
jrc8b8 <jrc8b8@umsystem.edu>
2021-05-18 13:53:18 -0500

    All Work So Far

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+//api.reddit.com/r/Planes
+
+//Postman prints out the content of a source website.
+
+//Asynchronous functions do not happen immediately.
+
+//JavaScript promises are asynchronous tasks that promise to be done eventually.
+
+//Two ways to handle promises
+//CALLBACKS
+//  Chained function of whens
+//ASYNC-AWAIT
+//  Tell process to pause.  Wait to resolve, and execute the rest of the code.
+
+
